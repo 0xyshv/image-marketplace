d2290d70f6ecad331a95b7872ec9533e916ed1cc
0xyshv <writetoyshv@gmail.com>
2023-09-11 14:38:16 +0530

    static posts display

--- public-square-app/src/components/NewPost.jsx
+++ public-square-app/src/components/NewPost.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import TextareaAutosize from 'react-textarea-autosize';
-import { arweave, getTopicString } from '../lib/api';
+import React from "react";
+import TextareaAutosize from "react-textarea-autosize";
+import { arweave, getTopicString } from "../lib/api";
 
 export const NewPost = (props) => {
+  const [topicValue, setTopicValue] = React.useState("");
   const [postValue, setPostValue] = React.useState("");
   const [isPosting, setIsPosting] = React.useState(false);
-
-  async function onPostButtonClicked() {
+  function onTopicChanged(e) {
+    let input = e.target.value;
+    let dashedTopic = getTopicString(input);
+    setTopicValue(dashedTopic);
   }
-
+  async function onPostButtonClicked() {
+    setIsPosting(true);
+    let tx = await arweave.createTransaction({ data: postValue });
+    tx.addTag("App-Name", "PublicSquare");
+    tx.addTag("Content-Type", "text/plain");
+    tx.addTag("Version", "1.0.1");
+    tx.addTag("Type", "post");
+    if (topicValue) {
+      tx.addTag("Topic", topicValue);
+    }
+    try {
+      let result = await window.arweaveWallet.dispatch(tx);
+      setPostValue("");
+      setTopicValue("");
+      if (props.onPostMessage) {
+        props.onPostMessage(result.id);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    setIsPosting(false);
+  }
   let isDisabled = postValue === "";
 
   if (props.isLoggedIn) {
@@ -16,58 +40,53 @@ export const NewPost = (props) => {
       return (
         <div className="newPost">
           <div className="newPostScrim" />
-          <TextareaAutosize
-            value={postValue}
-            readOnly={true}
-          />
+          <TextareaAutosize value={postValue} readOnly={true} />
           <div className="newPost-postRow">
-          {/* <div className="topic">
-              # 
+            <div className="topic">
+              #
               <input
-                type="text" 
+                type="text"
                 placeholder="topic"
                 className="topicInput"
                 value={topicValue}
                 disabled={true}
               />
-            </div> */}
-            <div >
-              <button 
-                className="submitButton"
-                disabled={true}
-              >
+            </div>
+            <div>
+              <button className="submitButton" disabled={true}>
                 Post
               </button>
             </div>
           </div>
         </div>
-      )
+      );
     } else {
       return (
         <div className="newPost">
           <TextareaAutosize
             value={postValue}
-            onChange={e => setPostValue(e.target.value)}
-            rows="1" 
-            placeholder="What do you have to say?" 
+            onChange={(e) => setPostValue(e.target.value)}
+            rows="1"
+            placeholder="What do you have to say?"
           />
           <div className="newPost-postRow">
-            {/* <div className="topic"
-              style={{color: topicValue  && "rgb( 80, 162, 255)" }}
+            <div
+              className="topic"
+              style={{ color: topicValue && "rgb( 80, 162, 255)" }}
             >
-              # 
+              #
               <input
-                type="text" 
+                type="text"
                 placeholder="topic"
                 className="topicInput"
                 value={topicValue}
-                onChange={e => onTopicChanged(e)}
+                onChange={(e) => onTopicChanged(e)}
               />
-            </div> */}
-            <div >
-              <button 
+            </div>
+            <div>
+              <button
                 className="submitButton"
-                disabled={isDisabled} 
+                disabled={isDisabled}
                 onClick={onPostButtonClicked}
               >
                 Post
@@ -75,9 +94,11 @@ export const NewPost = (props) => {
             </div>
           </div>
         </div>
-      )
+      );
     }
   } else {
-    return (<div className="darkRow">Connect your wallet to start posting...</div>)
+    return (
+      <div className="darkRow">Connect your wallet to start posting...</div>
+    );
   }
 };
--- public-square-app/src/components/ProfileButton.jsx
+++ public-square-app/src/components/ProfileButton.jsx
@@ -1,36 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import "./profileButton.css";
-import { account } from '../lib/api';
+import { account } from "../lib/api";
 
 export const ProfileButton = (props) => {
-	const [name, setName] = React.useState("");
-	const [handle, setHandle] = React.useState("");
-	const [src, setSrc] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [handle, setHandle] = React.useState("");
+  const [src, setSrc] = React.useState("");
 
-	React.useEffect(() => {
-		
-	}, [props.isWalletConnected]);
+  React.useEffect(() => {
+    if (props.isWalletConnected) {
+      const getAccount = async () => {
+        const activeAddress = await window.arweaveWallet.getActiveAddress();
+        const info = await account.get(activeAddress);
 
-	if (!src || src.length == 0) {
-		return <></>
-	} else {
+        if (info.txid != null) {
+          setName(info.profile.name);
+          setHandle(info.handle);
+          setSrc(info.profile.avatarURL);
+        } else {
+          setName(info.handle);
+          setHandle("edit account");
+          setSrc("img_avatar.png");
+        }
+      };
 
-		function onEditProfile() {
-			window.open("https://account.metaweave.xyz", '_blank');
-		}
+      getAccount();
+    } else {
+      setName("");
+      setHandle("");
+      setSrc("");
+    }
+  }, [props.isWalletConnected]);
 
-		return (
-			<button className="profileButton" onClick={onEditProfile}>
-				<div className="content">
-					<img className="profileImage" src={src} alt="profile icon" />
-					<div className="names">
-						<p>{name}</p>
-						{handle}
-					</div>
-					<div className="ellipse">
-						...
-					</div>
-				</div>
-			</button>)
-	}
-} 
+  if (!src || src.length == 0) {
+    return <></>;
+  } else {
+    function onEditProfile() {
+      window.open("https://account.metaweave.xyz", "_blank");
+    }
+
+    return (
+      <button className="profileButton" onClick={onEditProfile}>
+        <div className="content">
+          <img className="profileImage" src={src} alt="profile icon" />
+          <div className="names">
+            <p>{name}</p>
+            {handle}
+          </div>
+          <div className="ellipse">...</div>
+        </div>
+      </button>
+    );
+  }
+};
