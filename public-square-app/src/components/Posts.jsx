62dbd79afdbb9b8447c7f50659b2d1193a8d5e6a
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-09-12 16:59:25 +0530

    sync

--- public-square-app/src/components/Posts.jsx
+++ public-square-app/src/components/Posts.jsx
@@ -4,7 +4,7 @@ import { maxMessageLength, abbreviateAddress, getPostTime } from "../lib/api";
 
 export const Posts = (props) => {
   return (
-    <div>
+    <div className="grid grid-rows-3">
       {props.postInfos.map((postInfo) => (
         <PostItem key={postInfo.txid} postInfo={postInfo} />
       ))}
